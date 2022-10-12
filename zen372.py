e6d39abfb2dce7be7d9c7dcef72c45311698ff49
Usuario <usuario@gmail.com>
2022-10-11 20:13:44 -0700

    Agregando base para modulos

---
+++ zen372.py
@@ -0,0 +1,14 @@
+#import this
+# Comentario 1 linea
+
+"""
+Comentario multilinea
+"""
+
+print("Latosa1: Nose")
+print("Latosa2: Mas vale pedir perdon que pedir permiso")
+print("Marco1: Camaron que se duerme bla bla bla")
+print("Marco2: Fuga!!!!")
+print("Derek: Fuga x2")
+
+barba = "Algo que sorpresa!!!!"
